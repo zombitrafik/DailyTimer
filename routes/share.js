b4a076c8a96722385e1178c64a2ab51c2344b1e7
zombitrafik <zombitrafik05@gmail.com>
2015-07-20 21:44:02 +0300

    share types

--- routes/share.js
+++ routes/share.js
@@ -38,7 +38,17 @@ module.exports = function (router) {
 					time: items[i].time
 				});
 			}
-			return res.render('share.ejs', {schedule: result});
+			var type = req.query.type || '';
+			switch (type.toUpperCase()){
+				case 'WEB':
+					return res.render('share.ejs', {schedule: result});
+				break;
+				case 'MOBILE':
+					return res.json({schedule: result}); 
+				break;
+				default: 
+					return res.json({schedule: result});
+			}
 		});
 
 	});
